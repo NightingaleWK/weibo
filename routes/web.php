c0d05afc7213ee087a89a09e0fe43467dbacefad
NightingaleWK <ytzywk@163.com>
2022-09-27 14:45:43 +0800

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [StaticPagesController::class, 'home']);
-Route::get('/help', [StaticPagesController::class, 'help']);
-Route::get('/about',  [StaticPagesController::class, 'about']);
+Route::get('/', [StaticPagesController::class, 'home'])->name('home');
+Route::get('/help', [StaticPagesController::class, 'help'])->name('help');
+Route::get('/about',  [StaticPagesController::class, 'about'])->name('about');
