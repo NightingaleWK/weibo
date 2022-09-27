f1a02948c840572957ac8f3acb7941f69c9a9852
NightingaleWK <ytzywk@163.com>
2022-09-27 10:51:13 +0800

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StaticPagesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [StaticPagesController::class, 'home']);
+Route::get('/help', [StaticPagesController::class, 'help']);
+Route::get('/about',  [StaticPagesController::class, 'about']);
