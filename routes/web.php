7d0cbabfcb103feb5bafec6fc3e74613ccb05c68
NightingaleWK <ytzywk@163.com>
2022-09-27 17:36:57 +0800

    save master

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,3 @@ Route::get('/help', [StaticPagesController::class, 'help'])->name('help');
 Route::get('/about',  [StaticPagesController::class, 'about'])->name('about');
 
 Route::get('/signup',  [UsersController::class, 'create'])->name('signup');
-Route::resource('users',UsersController::class);
