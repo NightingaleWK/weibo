4d1c9b9bda1e9c11557fe814db868ad727c629f7
NightingaleWK <ytzywk@163.com>
2022-09-29 10:07:30 +0800

    管理员可删除用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'ytzywk';
         $user->email = 'djsywxsj@outlook.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
